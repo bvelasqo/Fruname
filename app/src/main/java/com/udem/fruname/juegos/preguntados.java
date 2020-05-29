f41df647b76a6f8ac707a1c2166d00bd10861a3a
Dianella <chzdiane22@gmail.com>
2020-05-29 10:36:49 -0500

    preguntados Santiago

--- app/src/main/java/com/udem/fruname/juegos/preguntados.java
+++ app/src/main/java/com/udem/fruname/juegos/preguntados.java
@@ -1,5 +1,6 @@
 package com.udem.fruname.juegos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,21 +12,35 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.udem.fruname.R;
 import com.udem.fruname.dbresources.DBHelper;
+import com.udem.fruname.tablero;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class preguntados extends AppCompatActivity {
 	Button opcion1, opcion2, opcion3;
 	TextView tvPreguntas;
-	String categoria, correcta;
+	String categoria, correcta,uid,nombre;
 	operacionesDB operacionesDB;
+	FirebaseAuth mAuth;
 	ArrayList<Pregunta> listaPreguntas;
 	ArrayList<Button> opciones;
+	DatabaseReference mDataBase;
+
 	Pregunta preguntaActual;
-	int contador;
+	private static final int XPAB=180;
+
+	int contador, numBoton,nivelActual,xpActual,scoreactual,puntos=0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +48,33 @@ public class preguntados extends AppCompatActivity {
 		setContentView(R.layout.activity_preguntados);
 		conectar();
 		opciones=new ArrayList<>();
+		mAuth = FirebaseAuth.getInstance();
+		mDataBase= FirebaseDatabase.getInstance().getReference();
+		uid=mAuth.getCurrentUser().getUid();
 		operacionesDB = new operacionesDB(getApplicationContext());
+		numBoton=getIntent().getIntExtra("boton",0);
 		categoria = getIntent().getStringExtra("categoria");
 		listaPreguntas = operacionesDB.getPreguntaPR(categoria);
 		actualizarPregunta();
+		traerDatos();
 		addBotones();
+	}
+
+	private void traerDatos() {
+		mDataBase.child("Jugadores").child(uid).addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+				scoreactual= Integer.parseInt(dataSnapshot.child("score").getValue().toString());
+				xpActual=Integer.parseInt(dataSnapshot.child("xp").getValue().toString());
+				nivelActual=Integer.parseInt(dataSnapshot.child("nivel").getValue().toString());
+				nombre = dataSnapshot.child("nombre").getValue().toString();
+			}
+
+			@Override
+			public void onCancelled(@NonNull DatabaseError databaseError) {
+
+			}
+		});
 	}
 
 	private void conectar() {
@@ -61,6 +98,14 @@ public class preguntados extends AppCompatActivity {
 	}
 
 	private void FinJuego() {
+		Map<String,Object> datos = new HashMap();
+		if(numBoton==nivelActual)
+			datos.put("nivel",nivelActual+1);
+		datos.put("xp",xpActual+XPAB);
+		datos.put("score",scoreactual+puntos);
+		mDataBase.child("Jugadores").child(uid).updateChildren(datos);
+		startActivity(new Intent(preguntados.this, tablero.class));
+		finish();
 	}
 
 	private void addBotones() {
@@ -74,6 +119,7 @@ public class preguntados extends AppCompatActivity {
 					if (correcta.equals(b.getText().toString())) {
 						Toast.makeText(getApplicationContext(), "¡Bien Hecho!¿Vamos por otra?", Toast.LENGTH_LONG).show();
 						b.setBackground(getResources().getDrawable(R.drawable.botonacertado));
+						puntos+=200;
 					} else {
 						Toast.makeText(getApplicationContext(), "No te desanimes juega otra vez!!, la respuesta es: " + correcta, Toast.LENGTH_LONG).show();
 						b.setBackground(getResources().getDrawable(R.drawable.botonincorrecto));
